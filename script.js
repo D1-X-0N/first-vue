cb41f0572bde9f5ffc21b485fd033ad8d08efe73
Dimaxiton <Dimaxiton5@yandex.ru>
2021-12-16 01:05:52 +0300

    smashed into pieces

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 "use strict";
 
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-const $cartBtn = document.querySelector(".cart-button");
-const $goodsList = document.querySelector('.goods-list');
-const $goodsItem = document.querySelector(".goods-item");
-const $searchBtn = document.querySelector(".search-button");
-const $searchInput = document.querySelector(".item-search");
+
+
+Vue.component("goods-list", {
+  props:["filtered"],
+  template: `<div class="goods-list">
+              <div class="goods-item" v-for="good of filtered" v-on:click="addItemCartList(good)">
+                <h3 >{{good.product_name}}</h3>
+                <p>\${{good.price}}</p>
+              </div>
+              <div class="goods-item"  v-if="filtered.length == 0">no data available</div>
+             </div> `,
+  methods: {
+    addItemCartList(good) {
+      fetch(`${API_URL}addToBasket.json`)
+        .then(() => {
+          this.$emit('add', good)
+        })      
+   }
+  }
+})
+
+Vue.component("cart-list", {
+  data() {
+    return {
+      visibleCart: true
+    }
+  },
+  props:['cartItems'],
+  template: `<div class="cart">
+              <button class="cart-button" type="button" @click="checkCart">Cart</button>
+              <div class="cart-list" v-if="visibleCart">
+                <div class="cart-item" v-for="item of cartItems" :key="item.id_product">
+                  <h3>{{item.product_name}}</h3>
+                  <p>{{item.price}}</p>
+                  <div class="cart-delete fas fa-times" v-on:click="removeItemCartList(item)"></div>
+                </div>
+                <div class="cart-sum">total amount: \${{getPrice()}}</div>  
+              </div>
+            </div>`,
+  methods: {
+    checkCart() {
+      this.visibleCart = !this.visibleCart;
+     },
+    removeItemCartList(item) {
+      fetch(`${API_URL}deleteFromBasket.json`)
+      .then(() => {
+        const delItem = this.cartItems.findIndex((goodItem) => goodItem.id_product == item.id_product);
+        this.cartItems.splice(delItem, 1);
+      })
+     },
+     getPrice() {
+      return this.cartItems.reduce((acm,item) => acm + item.price, 0)
+     },  
+  }
+})
+
+Vue.component("filterForm", {
+  data() {
+    return {
+      searchLine: ""
+    }
+  },
+  template: `<form action="#" class="search-form">
+              <button type="button" class="search-button" @click="filter(searchLine)">search</button>
+              <input type="text" class="item-search" @input="filter(searchLine)" v-model="searchLine">
+             </form>`,
+  methods: {
+    filter(src) {
+      this.$emit("filter", src)
+    }
+  }
+})
 
 const app = new Vue({
   el: "#app",
@@ -13,34 +80,19 @@ const app = new Vue({
     goods: [],
     filtered: [],
     cartItems: [],
-    searchLine: '',
-    visibleCart: true
   },
   methods: {
-   filter() {
-      this.searchLine = this.searchLine.trim();
-      if(this.searchLine.length == 0) {
+   filter(src) {
+      src = src.trim();
+      if(src.length == 0) {
         this.filtered = this.goods;
         return
       }
-      const reg = new RegExp(this.searchLine, "i");
+      const reg = new RegExp(src, "i");
       this.filtered = this.goods.filter((good) => reg.test(good.product_name));
    },
    addItemCartList(good) {
-      fetch(`${API_URL}addToBasket.json`)
-        .then(() => {
-          this.cartItems.push(good)
-        })      
-   },
-   removeItemCartList(item) {
-    fetch(`${API_URL}deleteFromBasket.json`)
-    .then(() => {
-      const delItem = this.cartItems.findIndex((goodItem) => goodItem.id_product == item.id_product);
-      this.cartItems.splice(delItem, 1);
-    })
-   },
-   getPrice() {
-    return this.cartItems.reduce((acm,item) => acm + item.price, 0)
+    this.cartItems.push(good)          
    },
    fetchGoods() {
     fetch(`${API_URL}catalogData.json`)
@@ -62,13 +114,11 @@ const app = new Vue({
       .catch((err) => { 
       console.error(err.text)
       })
-  },
-   checkCart() {
-    this.visibleCart = !this.visibleCart;
-   },
-  },
+  }
+},
   mounted() {
     this.fetchGoods();
     this.fetchItem();
   }
 });
+
